dd0fef79eff244c00d510fc2d26c3a68d3f4df8f
Zachary Capalbo <zach.geek@gmail.com>
2020-01-04 13:31:24 -0500

    Adjust grunge brush size

--- src/brush-list.js
+++ src/brush-list.js
@@ -6,7 +6,7 @@ const BrushList = [
   new ProceduralBrush({width: 20, height: 20, hardness: 0.9}),
   new ImageBrush('lines2', {width: 20, height: 20}),
   new ImageBrush('line_grunge2', {width: 20, height: 20}),
-  new ImageBrush('line_grunge1', {width: 20, height: 20, textured: true}),
+  new ImageBrush('line_grunge1', {width: 64, height: 16, textured: true}),
 ]
 
 export { BrushList }
