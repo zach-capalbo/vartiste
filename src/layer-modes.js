85684bb15d2a78d4b0b0f9d1efee9d6a39a37abf
Zachary Capalbo <zach.geek@gmail.com>
2020-03-24 07:43:13 -0400

    More FX shaders!

--- src/layer-modes.js
+++ src/layer-modes.js
@@ -45,7 +45,11 @@ const THREED_MODES = [
 const FX = [
   'blur',
   'noise',
-  "dither"
+  "dither",
+  'flip-x',
+  'flip-y',
+  'grayscale-to-alpha',
+  'alpha-to-grayscale'
 ]
 
 const LAYER_MODES = [].concat(BLEND_MODES, COLOR_MODES, MATH_MODES, THREED_MODES)
