468f828f11881eecee6fbbd527c60198858532b4
Zachary Capalbo <zach.geek@gmail.com>
2020-04-26 07:12:46 -0400

    Improve performance

--- src/ai-nodes.js
+++ src/ai-nodes.js
@@ -4,15 +4,13 @@ tf.ENV.set('WEBGL_PACK', false);  // This needs to be done otherwise things run 
 const STYLE_MODEL_PATH = require('./ai-models/saved_model_style_js/model.json')
 const TRANSFORM_MODEL_PATH = require('./ai-models/saved_model_transformer_separable_js/model.json')
 
-// for (let fileName of require.context('./ai-models/saved_model_style_js/', true, /.*/).keys()) {
-//   console.log(`./ai-models/saved_model_style_js/${fileName}`)
-//   require(`./ai-models/saved_model_style_js/${fileName}`)
-// }
+var requireAI = require.context('./ai-models', true, /./);
+requireAI.keys().forEach(requireAI);
 
-require('./ai-models/saved_model_style_js/group1-shard1of3')
-require('./ai-models/saved_model_style_js/group1-shard2of3')
-require('./ai-models/saved_model_style_js/group1-shard3of3')
-require('./ai-models/saved_model_transformer_separable_js/group1-shard1of1')
+// require('./ai-models/saved_model_style_js/group1-shard1of3')
+// require('./ai-models/saved_model_style_js/group1-shard2of3')
+// require('./ai-models/saved_model_style_js/group1-shard3of3')
+// require('./ai-models/saved_model_transformer_separable_js/group1-shard1of1')
 
 AFRAME.registerSystem('ai', {
   init() {
@@ -30,6 +28,7 @@ export class StyleTransferNode extends CanvasNode {
   constructor(compositor, {shader = "blur", ...opts} = {}) {
     super(compositor, opts)
     Object.defineProperty(this, "system", {enumerable: false, value: AFRAME.scenes[0].systems.ai})
+    Object.defineProperty(this, "data", {enumerable: false, value: {}})
 
   }
   disconnectDestination()
@@ -37,7 +36,8 @@ export class StyleTransferNode extends CanvasNode {
     this.destination = undefined
   }
   touch() {
-    this.updateTime = 0
+    super.touch()
+    this.data.styleEncodingTime = 0
   }
   async updateCanvas(frame) {
     if (!this.destination) return
@@ -45,32 +45,39 @@ export class StyleTransferNode extends CanvasNode {
 
     if (!this.checkIfUpdateNeeded(frame)) return
 
+    if (this.data.isRunningInference) return
     this.updateTime = document.querySelector('a-scene').time
 
     await this.runInference()
 
-    this.updateTime = document.querySelector('a-scene').time
   }
   async runInference() {
+    this.data.isRunningInference = true
     let {styleNet, transformNet} = this.system
     await tf.nextFrame()
 
-    let bottleneck = await tf.tidy(() => {
-      return styleNet.predict(tf.browser.fromPixels(this.sources[0].canvas).toFloat().div(tf.scalar(255)).expandDims());
-    })
+    if (!this.data.styleEncoding || this.sources[0].updateTime >= this.data.styleEncodingTime)
+    {
+      if (this.data.styleEncoding) this.data.styleEncoding.dispose()
+      let styleImage = this.sources[0].canvas
+      this.data.styleEncoding = await tf.tidy(() => {
+        this.data.styleEncodingTime = document.querySelector('a-scene').time
+        return styleNet.predict(tf.browser.fromPixels(styleImage).toFloat().div(tf.scalar(255)).expandDims());
+      })
+    }
 
     console.log("BottleNeck predicted")
 
     await tf.nextFrame();
     const stylized = await tf.tidy(() => {
-      return transformNet.predict([tf.browser.fromPixels(this.destination.canvas).toFloat().div(tf.scalar(255)).expandDims(), bottleneck]).squeeze();
+      return transformNet.predict([tf.browser.fromPixels(this.destination.canvas).toFloat().div(tf.scalar(255)).expandDims(), this.data.styleEncoding]).squeeze();
     })
 
     console.log("Stylized predicted")
 
 
     await tf.browser.toPixels(stylized, this.canvas);
-    bottleneck.dispose();  // Might wanna keep this around
     stylized.dispose();
+    this.data.isRunningInference = false
   }
 }
