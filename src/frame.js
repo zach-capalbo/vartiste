13614273d392bf937161b1f4428fa850cf4e028e
Zachary Capalbo <zach.geek@gmail.com>
2020-05-17 20:15:11 -0400

    Better pinning

--- src/frame.js
+++ src/frame.js
@@ -1,32 +1,75 @@
+import {Util} from './util.js'
+import {Pool} from './pool.js'
+
+AFRAME.registerSystem('frame', {
+  init() {
+    this.pinnedTargets = {}
+  },
+  pinFrameTo(frame, target) {
+    this.pinnedTargets[target.id] = this.pinnedTargets[target.id] || []
+    let targetList = this.pinnedTargets[target.id]
+
+    var nextIdx = -1; while (typeof targetList[++nextIdx] !== 'undefined');
+
+    targetList[nextIdx] = frame
+    return nextIdx
+  },
+  unpinFrameFrom(frame, target) {
+    this.pinnedTargets[target.id] = this.pinnedTargets[target.id] || []
+    let idx = this.pinnedTargets[target.id].indexOf(frame)
+
+    if (idx >= 0)
+    {
+      this.pinnedTargets[target.id][idx] = undefined
+    }
+  }
+})
+
 AFRAME.registerComponent("frame", {
-  dependencies: ['geometry'],
   schema: {
     closable: {default: true},
     pinnable: {default: true},
     outline: {default: true},
     outlineColor: {type: 'color', default: "#52402b"},
+    geometryTarget: {type: 'selector'}
   },
   events: {
     click: function (e) {
       if (e.target.hasAttribute('frame-action'))
       {
-        this[e.target.getAttribute('frame-action')]()
+        this[e.target.getAttribute('frame-action')](e)
       }
     }
   },
   init() {
-    let {width, height} = this.el.getAttribute('geometry')
+    Pool.init(this)
+    let {width, height} = (this.data.geometryTarget || this.el).getAttribute('geometry')
 
-    let closeButton = document.createElement('a-entity')
-    closeButton.setAttribute('icon-button', '#asset-close-circle-outline')
-    closeButton.setAttribute('button-style', 'buttonType: plane; depth: 0; color: #26211c')
-    closeButton.setAttribute('position', `${width / 2 - 0.055} ${height / 2 + 0.055} 0`)
-    closeButton.setAttribute('scale', `0.3 0.3 1`)
-    closeButton.setAttribute('frame-action', "closeFrame")
-    this.el.append(closeButton)
+    let buttonCount = 0
+
+    if (this.data.closable)
+    {
+      let closeButton = document.createElement('a-entity')
+      closeButton.setAttribute('icon-button', '#asset-close-circle-outline')
+      closeButton.setAttribute('button-style', 'buttonType: plane; color: #26211c')
+      closeButton.setAttribute('position', `${width / 2 - 0.055 - buttonCount++ * 0.6} ${height / 2 + 0.055} 0`)
+      closeButton.setAttribute('scale', `0.3 0.3 1`)
+      closeButton.setAttribute('frame-action', "closeFrame")
+      this.el.append(closeButton)
+    }
+
+    if (this.data.pinnable) {
+      let closeButton = document.createElement('a-entity')
+      closeButton.setAttribute('icon-button', '#asset-hand-right')
+      closeButton.setAttribute('button-style', 'buttonType: plane; color: #26211c')
+      closeButton.setAttribute('position', `${width / 2 - 0.055 - buttonCount++ * 0.6} ${height / 2 + 0.055} 0`)
+      closeButton.setAttribute('scale', `0.3 0.3 1`)
+      closeButton.setAttribute('frame-action', "pinFrame")
+      this.el.append(closeButton)
+    }
   },
   update(oldData) {
-    let {width, height} = this.el.getAttribute('geometry')
+    let {width, height} = (this.data.geometryTarget || this.el).getAttribute('geometry')
 
     if (this.data.outline && !this.lineObject)
     {
@@ -50,5 +93,59 @@ AFRAME.registerComponent("frame", {
   },
   closeFrame() {
     this.el.parentEl.removeChild(this.el)
+  },
+  pinFrame(e) {
+    if (this.el.is('pinned'))
+    {
+      this.system.unpinFrameFrom(this, this.el.object3D.parent.el)
+      this.el.object3D.parent.remove(this.el.object3D)
+      this.originalParent.add(this.el.object3D)
+      Util.applyMatrix(this.originalMatrix, this.el.object3D)
+      this.el.removeState('pinned')
+      return
+    }
+
+    let cursorEl = e.detail.cursorEl
+    if (!cursorEl) {
+      throw new Error("No cursor in event", e)
+    }
+
+    let {width, height} = (this.data.geometryTarget || this.el).getAttribute('geometry')
+    let scale = this.el.getAttribute('scale')
+
+    this.originalMatrix = this.originalMatrix || new THREE.Matrix4()
+    this.originalMatrix.copy(this.el.object3D.matrix)
+    this.originalParent = this.el.object3D.parent
+
+    let target
+    let offset = this.pool('offset', THREE.Vector3)
+    let pinSize = this.pool('pinSize', THREE.Vector3)
+    pinSize.set(0.1, 0.1, 0.1)
+    if (cursorEl.id === 'right-hand')
+    {
+      target = document.querySelector('#left-hand')
+      let pinIdx = this.system.pinFrameTo(this, target)
+      offset.set(0, height / 2 * scale.y * pinSize.y, 0)
+    }
+    else if (cursorEl.id === 'left-hand')
+    {
+      target = document.querySelector('#right-hand')
+      let pinIdx = this.system.pinFrameTo(this, target)
+      offset.set(0, height / 2 * scale.y  * pinSize.y, 0)
+    }
+    else
+    {
+      target = document.querySelector('#camera')
+      let pinIdx = this.system.pinFrameTo(this, target)
+      offset.set(-0.07 + pinIdx * 0.04, -0.07, -0.1)
+      pinSize.set(0.01, 0.01, 0.001)
+    }
+
+    this.el.object3D.parent.remove(this.el.object3D)
+    target.object3D.add(this.el.object3D)
+
+    Util.positionObject3DAtTarget(this.el.object3D, target.object3D, {scale: pinSize, transformOffset: offset})
+
+    this.el.addState('pinned')
   }
 })
