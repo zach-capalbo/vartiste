4ec8ba66df85e8b5d51b5782425e24db6f0a74c7
Zachary Capalbo <zach.geek@gmail.com>
2020-06-19 20:01:39 -0400

    New camera tool

--- src/camera-capture.js
+++ src/camera-capture.js
@@ -1,3 +1,5 @@
+import {Pool} from './pool.js'
+import {Util} from './util.js'
 AFRAME.registerSystem('camera-capture', {
   getTempCanvas() {
     let {width, height} = Compositor.component;
@@ -22,5 +24,52 @@ AFRAME.registerSystem('camera-capture', {
 
     canvas.getContext('2d').drawImage(this.el.sceneEl.canvas, 0, 0, canvas.width, canvas.height)
     return canvas
+  }
+})
+
+AFRAME.registerComponent('camera-tool', {
+  events: {
+    click: function(e) {
+      this.takePicture()
+    },
+  },
+  init() {
+    Pool.init(this)
+    this.el.classList.add('grab-root')
+
+
+    const depth = 0.1
+    const cameraWidth = 0.3
+    Util.whenLoaded(Compositor.el, () => {
+      let {width, height} = Compositor.el.getAttribute('geometry')
+      Compositor.el.object3D.updateMatrixWorld()
+      let scale = this.pool('scale', THREE.Vector3)
+      scale.setFromMatrixScale(Compositor.el.object3D.matrixWorld)
+      width = width * scale.x
+      height = height * scale.y
+
+      let camera = new THREE.PerspectiveCamera(45, width / height, 0.1, 10)
+      this.el.object3D.add(camera)
+
+      this.camera = camera
+
+      var helper = new THREE.CameraHelper( camera );
+      this.helper = helper
+      this.el.sceneEl.object3D.add( helper );
+
+      let body = document.createElement('a-box')
+      body.setAttribute('depth', depth)
+      body.setAttribute('width', cameraWidth)
+      body.setAttribute('height', height / width * cameraWidth)
+      body.setAttribute('propogate-grab', "")
+      body.setAttribute('position', `0 0 ${-depth / 2 - 0.001}`)
+      body.classList.add('clickable')
+      this.el.append(body)
+    })
+  },
+  takePicture() {
+    this.helper.visible = false
+    this.el.sceneEl.systems['camera-capture'].captureToCanvas(this.camera, Compositor.component.activeLayer.canvas)
+    this.helper.visible = true
   }
 })
