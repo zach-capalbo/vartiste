c818a47d147472f526ee15c206d22739b6bf0e0a
Zachary Capalbo <zach.geek@gmail.com>
2020-06-20 14:14:06 -0400

    Fix camera capture in VR

--- src/camera-capture.js
+++ src/camera-capture.js
@@ -18,11 +18,65 @@ AFRAME.registerSystem('camera-capture', {
     this.tempCanvas.height = height
     return this.tempCanvas
   },
-  captureToCanvas(camera, canvas) {
-    if (!canvas) canvas = this.getTempCanvas()
-    this.el.sceneEl.renderer.render(this.el.sceneEl.object3D, camera);
+  getTargetTempCanvas() {
+    let {width, height} = Compositor.component;
 
-    canvas.getContext('2d').drawImage(this.el.sceneEl.canvas, 0, 0, canvas.width, canvas.height)
+    width *= 3
+    height *= 3
+
+    if (this.targetTempCanvas) {
+      if (this.targetTempCanvas.width !== width || this.targetTempCanvas.height !== height)
+      {
+        this.targetTempCanvas.width = width
+        this.targetTempCanvas.height = height
+      }
+      return this.targetTempCanvas
+    }
+
+    this.targetTempCanvas = document.createElement('canvas')
+    this.targetTempCanvas.width = width
+    this.targetTempCanvas.height = height
+    return this.targetTempCanvas
+  },
+  captureToCanvas(camera, canvas) {
+    let renderer = this.el.sceneEl.renderer
+    let wasXREnabled = renderer.xr.enabled
+    renderer.xr.enabled = false
+
+    let oldTarget = renderer.getRenderTarget()
+
+    let {width, height} = canvas
+
+    let targetTempCanvas = this.getTargetTempCanvas()
+
+    let newTarget = new THREE.WebGLRenderTarget(targetTempCanvas.width, targetTempCanvas.height)
+
+    renderer.setRenderTarget(newTarget)
+
+    if (!canvas) canvas = this.getTempCanvas()
+
+    let ctx = targetTempCanvas.getContext('2d')
+
+    renderer.render(this.el.sceneEl.object3D, camera);
+
+    let data = ctx.getImageData(0, 0, targetTempCanvas.width, targetTempCanvas.height)
+
+    renderer.readRenderTargetPixels(newTarget, 0, 0, targetTempCanvas.width, targetTempCanvas.height, data.data)
+
+    ctx.putImageData(data, 0, 0)
+
+    let destCtx = canvas.getContext('2d')
+
+    destCtx.translate(canvas.width, canvas.height)
+    destCtx.scale(-1, -1)
+    destCtx.drawImage(targetTempCanvas, 0, 0, canvas.width, canvas.height)
+    destCtx.scale(-1, -1)
+    destCtx.translate(-canvas.width, -canvas.height)
+
+    renderer.xr.enabled = wasXREnabled
+
+    renderer.setRenderTarget(oldTarget)
+    newTarget.dispose()
     return canvas
   }
 })
@@ -36,7 +90,7 @@ AFRAME.registerComponent('camera-tool', {
   init() {
     Pool.init(this)
     this.el.classList.add('grab-root')
-
+    this.el.classList.add('clickable')
 
     const depth = 0.1
     const cameraWidth = 0.3
@@ -53,23 +107,39 @@ AFRAME.registerComponent('camera-tool', {
 
       this.camera = camera
 
-      var helper = new THREE.CameraHelper( camera );
-      this.helper = helper
-      this.el.sceneEl.object3D.add( helper );
-
       let body = document.createElement('a-box')
       body.setAttribute('depth', depth)
       body.setAttribute('width', cameraWidth)
       body.setAttribute('height', height / width * cameraWidth)
       body.setAttribute('propogate-grab', "")
       body.setAttribute('position', `0 0 ${-depth / 2 - 0.001}`)
+      body.setAttribute('material', 'src:#asset-shelf')
       body.classList.add('clickable')
       this.el.append(body)
+
+      Compositor.el.addEventListener('resized', (e) => {
+        let {width, height} = e.detail
+      })
     })
+
+    let activate = (e) => {
+      if (e.detail === 'grabbed') {
+        this.activate()
+        this.el.removeEventListener('stateadded', activate)
+      }
+    };
+    this.el.addEventListener('stateadded', activate)
   },
   takePicture() {
+    console.log("Taking picture")
     this.helper.visible = false
     this.el.sceneEl.systems['camera-capture'].captureToCanvas(this.camera, Compositor.component.activeLayer.canvas)
+    Compositor.component.activeLayer.touch()
     this.helper.visible = true
+  },
+  activate() {
+    var helper = new THREE.CameraHelper( this.camera );
+    this.helper = helper
+    this.el.sceneEl.object3D.add( helper );
   }
 })
