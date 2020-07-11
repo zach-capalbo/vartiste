b17d7da9a4b75e58af51aade5ad0f40c78cd4034
Zachary Capalbo <zach.geek@gmail.com>
2020-07-11 17:02:50 -0400

    Attempt to use 4 byte indexing

--- src/camera-capture.js
+++ src/camera-capture.js
@@ -42,7 +42,10 @@ AFRAME.registerSystem('camera-capture', {
     return this.targetTempCanvas
   },
   captureToCanvas(camera, canvas) {
-    if (!canvas) canvas = this.getTempCanvas()
+    if (!canvas) {
+      canvas = this.getTempCanvas()
+      canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+    }
 
     let renderer = this.el.sceneEl.renderer
     let wasXREnabled = renderer.xr.enabled
@@ -156,6 +159,122 @@ AFRAME.registerComponent('camera-tool', {
     this.helper.visible = false
     this.el.sceneEl.systems['camera-capture'].captureToCanvas(this.camera, Compositor.component.activeLayer.canvas)
     Compositor.component.activeLayer.touch()
+    this.helper.visible = true
+    this.el.sceneEl.emit("endsnap", {source: this.el})
+  },
+  activate() {
+    var helper = new THREE.CameraHelper( this.camera );
+    this.helper = helper
+    this.el.sceneEl.object3D.add( helper );
+  }
+})
+
+
+AFRAME.registerComponent('spray-can-tool', {
+  schema: {
+    orthographic: {default: false},
+    fov: {default: 5.0}
+  },
+  events: {
+    click: function(e) {
+      this.takePicture()
+    },
+  },
+  init() {
+    Pool.init(this)
+    this.el.classList.add('grab-root')
+    this.el.classList.add('clickable')
+
+    const depth = 0.1
+    const cameraWidth = 0.3
+    Util.whenLoaded(Compositor.el, () => {
+      let {width, height} = Compositor.el.getAttribute('geometry')
+      Compositor.el.object3D.updateMatrixWorld()
+      let scale = this.pool('scale', THREE.Vector3)
+      scale.setFromMatrixScale(Compositor.el.object3D.matrixWorld)
+      width = width * scale.x
+      height = height * scale.y
+
+      let camera;
+      if (this.data.orthographic)
+      {
+        camera = new THREE.OrthographicCamera(-width / 2, width / 2, height / 2, - height / 2, 0.1, 10)
+      }
+      else
+      {
+        camera = new THREE.PerspectiveCamera(this.data.fov, width / height, 0.1, 10)
+      }
+      this.el.object3D.add(camera)
+
+      this.camera = camera
+
+      let body = document.createElement('a-box')
+      body.setAttribute('depth', depth)
+      body.setAttribute('width', cameraWidth)
+      body.setAttribute('height', height / width * cameraWidth)
+      body.setAttribute('propogate-grab', "")
+      body.setAttribute('position', `0 0 ${-depth / 2 - 0.001}`)
+      body.setAttribute('material', 'src:#asset-shelf')
+      body.classList.add('clickable')
+      this.el.append(body)
+
+      Compositor.el.addEventListener('resized', (e) => {
+        let {width, height} = e.detail
+      })
+    })
+
+    let activate = (e) => {
+      if (e.detail === 'grabbed') {
+        this.activate()
+        this.el.removeEventListener('stateadded', activate)
+      }
+    };
+    this.el.addEventListener('stateadded', activate)
+  },
+  takePicture() {
+    console.log("Taking picture")
+    Undo.pushCanvas(Compositor.component.activeLayer.canvas)
+    this.el.sceneEl.emit("startsnap", {source: this.el})
+    this.helper.visible = false
+    let processor = new CanvasShaderProcessor({fx: 'uv-index'})
+    processor.setInputCanvas(Compositor.component.compositeCanvas)
+    processor.update()
+    Compositor.material.map.image = processor.canvas
+    Compositor.material.map.needsUpdate = true
+
+    let capturedImage = this.el.sceneEl.systems['camera-capture'].captureToCanvas(this.camera)
+
+    // TODO: Shaderize this
+    let capturedData = capturedImage.getContext("2d").getImageData(0, 0, capturedImage.width, capturedImage.height)
+    let targetCanvas = Compositor.component.activeLayer.canvas
+    let targetData = targetCanvas.getContext("2d").getImageData(0, 0, targetCanvas.width, targetCanvas.height)
+    for (let y = 0; y < capturedImage.height; y++)
+    {
+      for (let x = 0; x < capturedImage.width; x++)
+      {
+        let ulow = capturedData.data[((y * capturedImage.width) + x) * 4 + 0]
+        let uhigh = capturedData.data[((y * capturedImage.width) + x) * 4 + 1]
+        let vlow = capturedData.data[((y * capturedImage.width) + x) * 4 + 2]
+        let vhigh = capturedData.data[((y * capturedImage.width) + x) * 4 + 3]
+
+        let u = (ulow + (uhigh << 8)) / 65536
+        let v = (vlow + (vhigh << 8)) / 65536
+
+        let xx = Math.round(u * targetCanvas.width)
+        let yy = Math.round(v * targetCanvas.height)
+        if ((x + y * capturedData.width) % 1000 == 0)
+        {
+          console.log("UV", u, v, xx, yy, ulow, uhigh, vlow, vhigh)
+        }
+        targetData.data[[((yy * targetCanvas.width) + xx) * 4 + 0]] = 0
+        targetData.data[[((yy * targetCanvas.width) + xx) * 4 + 1]] = 100
+        targetData.data[[((yy * targetCanvas.width) + xx) * 4 + 2]] = 0
+        targetData.data[[((yy * targetCanvas.width) + xx) * 4 + 3]] = 255
+      }
+    }
+    targetCanvas.getContext("2d").putImageData(targetData, 0, 0)
+    Compositor.component.activeLayer.touch()
+
     this.helper.visible = true
     this.el.sceneEl.emit("endsnap", {source: this.el})
   },
--- src/shaders/fx/uv-index.glsl
+++ src/shaders/fx/uv-index.glsl
@@ -9,5 +9,13 @@ uniform sampler2D u_source_1;
 varying vec2 vUv;
 
 void main() {
-  gl_FragColor = vec4(vUv.x, vUv.y, 1.0, 1.0)
+  /* gl_FragColor = vec4(vUv.x, vUv.y, 1.0, 1.0) */
+/*
+  int u = int(vUv.x);
+  int v = int(vUv.y); */
+
+  float u = floor(vUv.x * 65535.0);
+  float v = floor(vUv.y * 65535.0);
+
+  gl_FragColor = vec4(mod(u, 256.0) / 255.0,  floor(u / 256.0) / 255.0, mod(v, 256.0) / 255.0, floor(v / 256.0) / 255.0);
 }
