95418fe8f2ee0f8f2be30e4b5fc5990ca4918d99
Zachary Capalbo <zach.geek@gmail.com>
2020-07-12 12:30:55 -0400

    Spray paint with brush

--- src/camera-capture.js
+++ src/camera-capture.js
@@ -176,22 +176,25 @@ AFRAME.registerComponent('spray-can-tool', {
     Pool.init(this)
     this.el.setAttribute('camera-tool', {fov: 1.5})
     this.takePicture = this.takePicture.bind(this.el.components['camera-tool'])
-    this.el.components['camera-tool'].takePicture = this.takePicture
+    this.el.components['camera-tool'].takePicture = this.takePicture;
 
-    this.cameraCanvas = document.createElement('canvas')
-    this.cameraCanvas.width = 30
-    this.cameraCanvas.height = 30
+    (function() {
+      this.cameraCanvas = document.createElement('canvas')
+      this.cameraCanvas.width = 30
+      this.cameraCanvas.height = 30
+      this.targetCanvas = document.createElement('canvas')
+      this.targetCanvas.width = 1024
+      this.targetCanvas.height = 512
+    }).call(this.el.components['camera-tool'])
   },
   takePicture() {
+    let startTime = Date.now()
     console.log("Taking picture")
-    Undo.pushCanvas(Compositor.component.activeLayer.canvas)
     this.el.sceneEl.emit("startsnap", {source: this.el})
     this.helper.visible = false
-    // let processor = new CanvasShaderProcessor({fx: 'uv-index'})
-    // processor.setInputCanvas(Compositor.component.compositeCanvas)
-    // processor.update()
-    // Compositor.material.map.image = processor.canvas
-    // Compositor.material.map.needsUpdate = true
+
+    let brush = this.el.sceneEl.systems['paint-system'].brush
+    let color = brush.color3
 
     let oldMaterial = Compositor.material
     let shaderMaterial = new THREE.ShaderMaterial({
@@ -204,10 +207,25 @@ AFRAME.registerComponent('spray-can-tool', {
     // this.cameraCanvas.clearRect(0, 0, this.cameraCanvas.width, this.cameraCanvas.height)
     let capturedImage = this.el.sceneEl.systems['camera-capture'].captureToCanvas(this.camera, this.cameraCanvas)
 
+    let pictureTime = Date.now() - startTime
+
     // TODO: Shaderize this
     let capturedData = capturedImage.getContext("2d").getImageData(0, 0, capturedImage.width, capturedImage.height)
-    let targetCanvas = Compositor.component.activeLayer.canvas
+    let targetCanvas = this.targetCanvas
+    let finalDestinationCanvas = Compositor.component.activeLayer.canvas
+
+    if (targetCanvas.width !== finalDestinationCanvas.width || targetCanvas.height !== finalDestinationCanvas.height)
+    {
+      targetCanvas.width = finalDestinationCanvas.width
+      targetCanvas.height = finalDestinationCanvas.height
+    }
+    targetCanvas.getContext("2d").clearRect(0, 0, targetCanvas.width, targetCanvas.height)
+
     let targetData = targetCanvas.getContext("2d").getImageData(0, 0, targetCanvas.width, targetCanvas.height)
+
+    let brushData = brush.overlayCanvas.getContext("2d").getImageData(0, 0, brush.width, brush.height)
+
+    let imageDataTime = Date.now() - startTime - pictureTime
     for (let y = 0; y < capturedImage.height; y++)
     {
       for (let x = 0; x < capturedImage.width; x++)
@@ -216,30 +234,41 @@ AFRAME.registerComponent('spray-can-tool', {
         let g = capturedData.data[((y * capturedImage.width) + x) * 4 + 1]
         let b = capturedData.data[((y * capturedImage.width) + x) * 4 + 2]
 
-
+        let bx = Math.floor(x / capturedImage.width * brush.width)
+        let by = Math.floor(y / capturedImage.height * brush.height)
 
         let u = (((b & 0xF0) >> 4) * 256 + r) / 4096
         let v = ((b & 0x0F) * 256 + g) / 4096
 
         let xx = Math.round(u * targetCanvas.width)
         let yy = Math.round(v * targetCanvas.height)
-        if ((x + y * capturedData.width) % 1000 == 0)
-        {
-          // console.log("UV", u, v, xx, yy, r,g,b)
-        }
-        targetData.data[[((yy * targetCanvas.width) + xx) * 4 + 0]] = 0
-        targetData.data[[((yy * targetCanvas.width) + xx) * 4 + 1]] = 100
-        targetData.data[[((yy * targetCanvas.width) + xx) * 4 + 2]] = 0
-        targetData.data[[((yy * targetCanvas.width) + xx) * 4 + 3]] = 255
+
+
+        targetData.data[((yy * targetCanvas.width) + xx) * 4 + 0] = brush.color3.r * 255
+        targetData.data[((yy * targetCanvas.width) + xx) * 4 + 1] = brush.color3.g * 255
+        targetData.data[((yy * targetCanvas.width) + xx) * 4 + 2] = brush.color3.b * 255
+        targetData.data[((yy * targetCanvas.width) + xx) * 4 + 3] = brushData.data[((by * brush.overlayCanvas.width) + bx) * 4 + 3]
       }
     }
+    let mathTime = Date.now() - startTime - pictureTime - imageDataTime
     targetCanvas.getContext("2d").putImageData(targetData, 0, 0)
+
+    let finalContext = finalDestinationCanvas.getContext("2d")
+    let oldAlpha = finalContext.globalAlpha
+    finalContext.globalAlpha = brush.opacity
+    finalContext.drawImage(targetCanvas, 0, 0)
+    finalContext.globalAlpha = oldAlpha
+
     Compositor.component.activeLayer.touch()
+
+    let drawTime = Date.now() - startTime - pictureTime
 
     Compositor.mesh.material = oldMaterial
 
     this.helper.visible = true
     this.el.sceneEl.emit("endsnap", {source: this.el})
+
+    console.log("Took", Date.now() - startTime, pictureTime, drawTime, imageDataTime, mathTime)
   },
   activate() {
     var helper = new THREE.CameraHelper( this.camera );
