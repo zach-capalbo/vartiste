b983aa9dc576e207d56e06fcad4ba010478936e9
Zachary Capalbo <zach.geek@gmail.com>
2020-11-17 21:27:27 -0500

    Fix uv material boxes for indexed geometry, and add VRM import

--- src/file-upload.js
+++ src/file-upload.js
@@ -102,7 +102,7 @@ async function addGlbViewer(file, {postProcessMesh = true} = {}) {
 
   if (document.querySelector('a-scene').systems['settings-system'].projectName === 'vartiste-project')
   {
-    document.querySelector('a-scene').systems['settings-system'].setProjectName(file.name.replace(/\.(glb|obj)$/i, ""))
+    document.querySelector('a-scene').systems['settings-system'].setProjectName(file.name.replace(/\.(glb|obj|vrm)$/i, ""))
   }
 
   let startingLayerLength = compositor.layers.length
@@ -115,6 +115,7 @@ async function addGlbViewer(file, {postProcessMesh = true} = {}) {
   {
     case '.obj': format = 'obj'; break
     case '.fbx': format = 'fbx'; break
+    case '.vrm': format = 'vrm'; break
   }
 
   let model
@@ -156,6 +157,7 @@ async function addGlbViewer(file, {postProcessMesh = true} = {}) {
   })
 
   model.scene.traverse(o => {
+    if (o.materials || (o.material && o.material.length)) console.warn("Multimaterial!", o, o.material)
     if (o.material)
     {
       materials[materialId(o.material)] = o.material
@@ -229,29 +231,50 @@ async function addGlbViewer(file, {postProcessMesh = true} = {}) {
       }
     }
 
+    // Prevent double adjustment
+    let hasAdjustedGeometry = {}
+    let adjustedArrays = new Set()
+
     if (combineMaterials)
     {
       model.scene.traverse(o => {
 
         if (o.material && o.geometry.attributes.uv)
         {
+          if (hasAdjustedGeometry[o.geometry.uuid]) return;
+
           let attr = o.geometry.attributes.uv
           let geometry = o.geometry
           let currentBox = materialBoxes[materialId(o.material)]
+
           //geometry = geometry.toNonIndexed()
 
           if (attr.data)
           {
+            if (adjustedArrays.has(attr.data)) return;
+
             for (let i = 0; i < attr.count; ++i)
             {
               attr.setXY(i,
                 THREE.Math.mapLinear(attr.getX(i) % 1.00000000000001, 0, 1, currentBox.min.x, currentBox.max.x),
                 THREE.Math.mapLinear(attr.getY(i) % 1.00000000000001, 0, 1, currentBox.min.y, currentBox.max.y))
             }
+
+            adjustedArrays.add(attr.data)
           }
           else
           {
+            if (adjustedArrays.has(geometry.attributes.uv.array)) return;
+
+            let indices = {has: function() { return true; }}
+            if (geometry.index)
+            {
+              indices = new Set(geometry.index.array)
+            }
+
             for (let i in geometry.attributes.uv.array) {
+              if (!indices.has(Math.floor(i / 2))) continue;
+
               if (i %2 == 0) {
                 attr.array[i] = THREE.Math.mapLinear(attr.array[i] % 1.00000000000001, 0, 1, currentBox.min.x, currentBox.max.x)
               }
@@ -260,9 +283,12 @@ async function addGlbViewer(file, {postProcessMesh = true} = {}) {
                 attr.array[i] = THREE.Math.mapLinear(attr.array[i] % 1.00000000000001, 0, 1, currentBox.min.y, currentBox.max.y)
               }
             }
+
+            if (!geometry.index) adjustedArrays.add(geometry.attributes.uv.array)
           }
           //o.geometry = geometry
           geometry.attributes.uv.needsUpdate = true
+          hasAdjustedGeometry[geometry.uuid] = true
         }
       })
 
@@ -321,6 +347,11 @@ async function addGlbViewer(file, {postProcessMesh = true} = {}) {
       catch (e) {
         console.error("Could not bake vertex colors", e)
       }
+    }
+
+    if (format === 'vrm')
+    {
+      Compositor.meshRoot.parent.rotation.set(0, Math.PI, 0)
     }
 
     compositor.activateLayer(startingLayer);
@@ -429,7 +460,7 @@ Util.registerComponentSystem('file-upload', {
       return
     }
 
-    if (/\.(glb)|(gltf)|(obj)|(fbx)$/i.test(file.name))
+    if (/\.(glb)|(gltf)|(obj)|(fbx)|(vrm)$/i.test(file.name))
     {
       if (settings.data.addReferences)
       {
