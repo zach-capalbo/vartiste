58057c62041030700ff3edd1977cf3067269868d
Zachary Capalbo <zach.geek@gmail.com>
2020-12-31 05:09:29 -0500

    Refactored crash handler

--- src/crash-handler.js
+++ src/crash-handler.js
@@ -34,14 +34,19 @@ AFRAME.registerSystem('crash-handler', {
     })
 
     this.handleCrash = this._handleCrash
+
+    let oldRender = this.el.sceneEl.render;
+    this.el.sceneEl.render = () => { try { oldRender(); } catch (e) {this._handleCrash(e)} }
   },
   tick(t, dt) {
     this.numberOfBygoneTicks = 0
     if (dt < 50 && !this.interval && document.visibilityState === "visible") this.installCrashCheck()
-    if (this.crashShown) this.clearCrash()
+    // if (this.crashShown) this.clearCrash()
     if (this.shouldCrash) throw new Error("Simulated VARTISTE crash")
   },
   installCrashCheck() {
+    // Disabling since it doesn't work / is no longer needed?
+    return;
     console.log("Installing check")
     if (!this.interval) {
       this.interval = window.setInterval(() => {
@@ -49,9 +54,11 @@ AFRAME.registerSystem('crash-handler', {
       }, 500)
     }
   },
-  _handleCrash() {
+  _handleCrash(e) {
+    console.error(e)
     if (this.crashShown) return
     console.error("Handling crash. What to do what to do 😱");
+    this.noticeDiv.querySelector('#crash-stack-trace').innerText = ">> " + e.toString() + "\n\n" + e.stack
     this.noticeDiv.classList.remove("hidden")
     this.hasCrashed = true
     this.crashShown = true
