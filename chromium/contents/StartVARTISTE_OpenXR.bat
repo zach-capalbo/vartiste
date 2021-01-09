ee60663d05d58bf657a9dad12f8d048b1755ff39
Zachary Capalbo <zach.geek@gmail.com>
2021-01-09 16:05:48 -0500

    Fix chrome shortcuts

--- chromium/contents/StartVARTISTE_OpenXR.bat
+++ chromium/contents/StartVARTISTE_OpenXR.bat
@@ -1,1 +1,1 @@
-.\chromium\vartiste-chromium.exe --disable-features=XRSandbox --force-webxr-runtime=OpenXR --autoplay-policy=no-user-gesture-required --user-data-dir=%APPDATA%\vartiste --app=https://vartiste.xyz %*
+.\chromium-vartiste\chrome.exe --disable-features=XRSandbox --force-webxr-runtime=OpenXR --autoplay-policy=no-user-gesture-required --user-data-dir=%APPDATA%\vartiste --app=https://vartiste.xyz %*
