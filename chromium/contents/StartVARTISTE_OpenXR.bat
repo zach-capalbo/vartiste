87c8ba9431278c9e7e6f555b5e70b7cac04e11de
Zachary Capalbo <zach.geek@gmail.com>
2021-01-09 15:42:02 -0500

    Update Chromium shortcuts

---
+++ chromium/contents/StartVARTISTE_OpenXR.bat
@@ -0,0 +1,1 @@
+.\chromium\vartiste-chromium.exe --disable-features=XRSandbox --force-webxr-runtime=OpenXR --autoplay-policy=no-user-gesture-required --user-data-dir=%APPDATA%\vartiste --app=https://vartiste.xyz %*
