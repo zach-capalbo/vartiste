075fcb8e7fcde3899dd9bdc5795015f232875acb
Zachary Capalbo <zach.geek@gmail.com>
2021-06-11 18:55:45 -0400

    Don't use val-loader for build info

--- src/about-shelf.js
+++ src/about-shelf.js
@@ -1,4 +1,12 @@
-const buildInfo = require('!!val-loader!./build-info.js')
+var buildInfo;
+
+try {
+ buildInfo = require('./built-info.js')
+}
+catch (e)
+{
+ buildInfo = require('!!val-loader!./build-info.js')
+}
 
 AFRAME.registerSystem('vartiste-version-info', {
   init() {
@@ -9,7 +17,7 @@ AFRAME.registerSystem('vartiste-version-info', {
 AFRAME.registerComponent('version-info', {
   dependencies: ['text'],
   init() {
-    let versionText = `//${buildInfo.version} - ${new Date(buildInfo.date).toISOString().split('T')[0]}`
+    let versionText = `${buildInfo.version} - ${new Date(buildInfo.date).toISOString().split('T')[0]}`
     this.el.setAttribute('text', {value: versionText})
   }
 })
