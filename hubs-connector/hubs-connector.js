f4d450618c2b5c1e604de64a64a635d0efb00608
Zachary Capalbo <zach.geek@gmail.com>
2021-06-20 09:52:30 -0400

    Use preset HDRI by default

--- hubs-connector/hubs-connector.js
+++ hubs-connector/hubs-connector.js
@@ -73,6 +73,23 @@ class VartisteHubsConnector extends HubsBot {
       )
     }, tool);
   }
+  async fetchToolLocation() {
+    return await this.evaluate(() => {
+      this.pencilMatrix = this.pencilMatrix || new THREE.Matrix4()
+
+      let rigObj = document.querySelector('#avatar-rig').object3D
+      rigObj.updateMatrixWorld()
+      this.pencilMatrix.copy(rigObj.matrixWorld)
+      this.pencilMatrix.invert()
+
+      this.pencil.object3D.updateMatrix()
+      this.pencilMatrix.premultiply(this.pencil.object3D.matrix)
+
+      return this.pencilMatrix.elements;
+
+      // this.pencilMatrix.premultiply(rigObj.matrixWorld)
+    })
+  }
 }
 
 let bot = new VartisteHubsConnector({
