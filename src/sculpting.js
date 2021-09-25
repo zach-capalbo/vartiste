bf6a0916f52f2d7926537a411073b411ac6b53ce
Zachary Capalbo <zach.geek@gmail.com>
2021-09-25 10:46:56 -0400

    Point to point sculpting tool

--- src/sculpting.js
+++ src/sculpting.js
@@ -763,6 +763,7 @@ AFRAME.registerComponent('threed-line-tool', {
   schema: {
     meshContainer: {type: 'selector', default: '#world-root'},
     switchbackAngle: {default: 80.0},
+    pointToPoint: {default: false},
   },
   events: {
     activate: function(e) {
@@ -786,7 +787,60 @@ AFRAME.registerComponent('threed-line-tool', {
       })
       this.createMesh(this.points)
     },
+    triggerdown: function(e) {
+      if (!this.data.pointToPoint) return;
+      let tipWorld = this.pool('tipWorld', THREE.Vector3)
+      this.tipPoint.getWorldPosition(tipWorld)
+      this.points.push({
+        x: tipWorld.x,
+        y: tipWorld.y,
+        z: tipWorld.z,
+        fx: 0,
+        fy: 0,
+        fz: 1,
+        scale: 1
+      })
+
+      this.points.push({
+        x: tipWorld.x,
+        y: tipWorld.y,
+        z: tipWorld.z,
+        fx: 0,
+        fy: 0,
+        fz: 1,
+        scale: 1
+      })
+
+      this.tiggerConstraint = this.el.sceneEl.systems.manipulator.installConstraint(this.el, () => {
+        this.tipPoint.getWorldPosition(tipWorld)
+
+        this.points[1].x = tipWorld.x
+        this.points[1].y = tipWorld.y
+        this.points[1].z = tipWorld.z
+
+        let worldScale = this.pool('worldScale', THREE.Vector3)
+        this.tipPoint.getWorldDirection(this.worldForward)
+        this.tipPoint.getWorldPosition(tipWorld)
+
+        for (let i = 0; i <= 1; ++i)
+        {
+          this.points[i].fx = this.worldForward.x
+          this.points[i].fy = this.worldForward.y
+          this.points[i].fz = this.worldForward.z
+          scale: Math.pow(0.8 * this.el.object3D.scale.x / this.initialScale, 2)
+        }
+
+        this.createMesh(this.points)
+      })
+    },
+    triggerup: function(e) {
+      console.log("Trigger up")
+      if (!this.data.pointToPoint) return;
+      this.doneDrawing()
+      this.el.sceneEl.systems.manipulator.removeConstraint(this.el, this.tiggerConstraint)
+    },
     draw: function(e) {
+      if (this.data.pointToPoint) return;
       if (!this.endDrawingEl)
       {
         this.endDrawingEl = e.detail.sourceEl;
@@ -879,19 +933,21 @@ AFRAME.registerComponent('threed-line-tool', {
     this.el.classList.add('grab-root')
     this.handle = this.el.sceneEl.systems['pencil-tool'].createHandle({radius: 0.05, height: 0.5, segments: 8, parentEl: this.el})
     let tipHeight = 0.3
-    let tip = this.tip = document.createElement('a-cone')
+    let tip = this.tip = this.data.pointToPoint ? document.createElement('a-sphere') : document.createElement('a-cone')
     this.el.append(tip)
     tip.setAttribute('radius-top', 0)
     tip.setAttribute('radius-bottom',  0.05)
-    tip.setAttribute('segments-height', 2)
+    tip.setAttribute('radius',  0.05)
+    tip.setAttribute('segments-height', this.data.pointToPoint ? 6 : 2)
     tip.setAttribute('segments-radial', 8)
     tip.setAttribute('height', tipHeight)
     tip.setAttribute('position', `0 ${tipHeight / 2.0} 0`)
     tip.setAttribute('material', 'shader: matcap; src: #asset-shelf')
     let tipPoint = this.tipPoint = new THREE.Object3D();
     tip.object3D.add(tipPoint);
-    tipPoint.position.set(0, tipHeight / 2.0, 0);
+    tipPoint.position.set(0, this.data.pointToPoint ? 0 : tipHeight / 2.0, 0);
     tipPoint.rotation.set(- Math.PI / 2, 0, 0);
+    this.el.sceneEl.systems['button-caster'].install(['trigger'])
 
     this.el.setAttribute('six-dof-tool', 'orientation', new THREE.Vector3(0, 1, 0))
 
@@ -1097,7 +1153,8 @@ AFRAME.registerComponent('threed-line-tool', {
     if (brush instanceof StretchBrush)
     {
       canvas = brush.image
-      color = brush.color3
+      color = new THREE.Color(brush.color3)
+      color.convertSRGBToLinear()
       opacity = brush.opacity
 
       if (!Util.isLowPower())
