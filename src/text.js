e42bad593c6db112bb7bd324c6cc1cc3ac21fe93
Zachary Capalbo <zach.geek@gmail.com>
2021-10-05 04:20:44 -0400

    Include troika text shim in toolkit

--- src/text.js
+++ src/text.js
@@ -1,7 +1,17 @@
 require('aframe-troika-text');
 
-((replaceText) => {
+const replaceText = (() => {
+  if (window.VARTISTE_TOOLKIT) return window.VARTISTE_TOOLKIT.replaceTextWithTroikaText;
+  return true
+})();
+
+;((replaceText) => {
 if (!replaceText) return
+
+if (window.VARTISTE_TOOLKIT) {
+  console.info("VARTISTE TOOLKIT NEW BEHAVIOR NOTICE: Replacing AFRAME text component with TroikaText shim. Set VARTISTE_TOOLKIT.replaceTextWithTroikaText = false to disable this")
+}
+
 const TEXT_SCHEMA = AFRAME.components['text'].schema
 delete AFRAME.components.text
 
@@ -9,7 +19,8 @@ AFRAME.registerComponent('text', {
   dependencies: ['troika-text'],
   schema: Object.assign({}, TEXT_SCHEMA,{
     // font: {default: "https://fonts.gstatic.com/s/nunito/v14/XRXW3I6Li01BKofA6sKUYevO.woff"}
-    font: {default: require('./assets/font.woff')}
+    font: {default: window.VARTISTE_TOOLKIT_URL ? `${VARTISTE_TOOLKIT_URL}/${require('./assets/font.woff')}`
+                                                : require('./assets/font.woff')}
   }),
   update(oldData) {
     let troikaData = {}
@@ -114,4 +125,20 @@ AFRAME.registerComponent('text', {
 })
 
 // AFRAME.components['text'] = AFRAME.components['vartiste-text']
-})(true)
+})(replaceText)
+
+
+AFRAME.registerComponent('text-geometry-shifter', {
+  dependencies: ['text'],
+  init() {
+    if (replaceText) return;
+    let oldUpdateLayout = this.el.components.text.updateLayout;
+    this.el.components.text.updateLayout = function(...args) {
+      oldUpdateLayout.call(this, ...args)
+      let data = this.el.getAttribute('text')
+      let height = this.el.getAttribute('geometry').height
+      if (data.baseline === 'top') { this.el.getObject3D('mesh').position.y = - height / 2 / 1.1}
+      else if (data.baseline === 'bottom') { this.el.getObject3D('mesh').position.y = height / 2 / 1.1}
+    }
+  }
+})
