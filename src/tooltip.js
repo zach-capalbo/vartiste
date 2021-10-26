52d05f59c1da0822a71e1f423a5c4e1d6c0569e4
cpmcclure <c.patrick.mcclure@gmail.com>
2021-10-26 16:55:44 +0000

    Updated tooltip-style description

--- src/tooltip.js
+++ src/tooltip.js
@@ -99,7 +99,7 @@ AFRAME.registerComponent('tooltip', {
   }
 })
 
-// Allows you to change the style of the [`tooltip`](#tooltip) component somewhat
+// Allows manipulation of [`tooltip`](#tooltip) size, placement and style.
 AFRAME.registerComponent('tooltip-style', {
   dependencies: ["tooltip"],
   multiple: true,
