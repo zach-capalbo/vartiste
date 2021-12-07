3e76e7fbb81a27be2efecd1c8d429fddb811a8d0
Zachary Capalbo <zach.geek@gmail.com>
2021-12-07 18:59:26 -0500

    No pool logging

--- src/pool.js
+++ src/pool.js
@@ -3,14 +3,14 @@ var poolSize = 0
 function sysPool(name, type) {
     if (this.system._pool[name]) return this.system._pool[name]
     this.system._pool[name] = new type()
-    console.log("SysPooling", type.name)
+    // console.log("SysPooling", type.name)
     return this.system._pool[name]
 }
 
 function pool(name, type) {
     if (this._pool[name]) return this._pool[name]
     this._pool[name] = new type()
-    console.log("Pooling", type.name)
+    // console.log("Pooling", type.name)
     return this._pool[name]
 }
 
