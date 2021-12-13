bc79abc021ac188f959b91b0658918efd3bdf4b8
Zachary Capalbo <zach.geek@gmail.com>
2021-12-13 14:14:07 -0500

    Make sure to load hand controls

--- src/index.js
+++ src/index.js
@@ -138,15 +138,17 @@ if (!AFRAME.utils.device.isOculusBrowser() && typeof WebXRLayersPolyfill !== 'un
 }
 AFRAME.registerSystem('vartiste-init', {
   play() {
-  document.getElementById('right-hand').setAttribute('right-hand-controls', "")
-  document.getElementById('left-hand').setAttribute('left-hand-controls', "")
+    Util.whenLoaded(this.el.sceneEl, () => {
+      document.getElementById('right-hand').setAttribute('right-hand-controls', "")
+      document.getElementById('left-hand').setAttribute('left-hand-controls', "")
+    })
 
-  document.addEventListener('keydown', e => {
-    if (e.key == "r") document.querySelector('a-scene').systems['artist-root'].resetCameraLocation()
-  })
+    document.addEventListener('keydown', e => {
+      if (e.key == "r") document.querySelector('a-scene').systems['artist-root'].resetCameraLocation()
+    })
 
-  document.getElementById('got-it').addEventListener('click', e => {
-    document.getElementById('need-help-notification').classList.add('hidden')
-  })
-}
+    document.getElementById('got-it').addEventListener('click', e => {
+      document.getElementById('need-help-notification').classList.add('hidden')
+    })
+  }
 })
