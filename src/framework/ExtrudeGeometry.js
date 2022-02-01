ab7f6ff85c2a721413736700bc856a6a65f429fa
Zachary Capalbo <zach.geek@gmail.com>
2022-01-31 19:02:14 -0500

    Fix extrude wand resizing

--- src/framework/ExtrudeGeometry.js
+++ src/framework/ExtrudeGeometry.js
@@ -211,7 +211,7 @@ class ExtrudeGeometry extends BufferGeometry {
         shapeLengths[i] = shapeLengths[i - 1] + vertices[i].distanceTo(vertices[i - 1])
       }
 
-      shapeLengths[vlen] = shape.getLength()
+      shapeLengths[vlen] = shapeLengths[vlen - 1] + vertices[0].distanceTo(vertices[vlen - 1])
 
       console.log("Shape", vertices, shapeLengths, shape.getLength())
 
